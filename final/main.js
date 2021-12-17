55c0341b8e29fd201535d56d05b2d4e94981b3e9
Daniel Goncalves <daniel.goncalves2500@gmail.com>
2021-12-16 19:58:26 -0500

    update

--- final/main.js
+++ final/main.js
@@ -79,8 +79,8 @@ class Game {
     // var groundMaterial = new CANNON.Material();
 
     this.obj = {};
-    this.mario = await this.getMarioModel();
     this.world = await this.getWorldModel();
+    await this.getMarioModel();
     this.loadModelIntoScene();
 
     this.RAF();
@@ -90,24 +90,31 @@ class Game {
   async getMarioModel() {
     const loader = new FBXLoader();
     loader.setPath("./resources/mario/");
-    const anim = new FBXLoader();
-    anim.setPath("./resources/mario/animations/");
-    var [idle, run, jump] = await Promise.all([
-      loader.loadAsync("idle.fbx"),
-      anim.loadAsync("run.fbx"),
-      anim.loadAsync("jump.fbx"),
-    ]);
-    console.log(jump);
-    this.animations.push(
-      idle.animations[0],
-      run.animations[0],
-      jump.animations[0]
-    );
-    this.currentMixer = new THREE.AnimationMixer(idle);
-    this.animationAction = this.currentMixer.clipAction(idle.animations[0]);
-    this.animationAction.play();
+    // const anim = new FBXLoader();
+    // anim.setPath("./resources/mario/animations/");
+    // var [idle, run, jump] = await Promise.all([
+    //   loader.loadAsync("idle.fbx"),
+    //   anim.loadAsync("run.fbx"),
+    //   anim.loadAsync("jump.fbx"),
+    // ]);
+    // console.log(jump);
+    // this.animations.push(
+    //   idle.animations[0],
+    //   run.animations[0],
+    //   jump.animations[0]
+    // );
+    // this.currentMixer = new THREE.AnimationMixer(idle);
+    // this.animationAction = this.currentMixer.clipAction(idle.animations[0]);
+    // this.animationAction.play();
+    const model = await loader.loadAsync("idle.fbx");
+    console.log(model);
+    model.scale.set(6, 6, 6);
+    model.position.set(-100, 10, 20);
+    model.rotation.y = 1.5;
+    this.scene.add(model);
 
-    return idle;
+    return;
+    // return idle;
   }
 
   async getWorldModel() {
@@ -119,10 +126,10 @@ class Game {
 
   loadModelIntoScene() {
     // Configure and load mario
-    this.mario.scale.set(6, 6, 6);
-    this.mario.position.set(-100, 10, 20);
-    this.mario.rotation.y = 1.5;
-    this.scene.add(this.mario);
+    // this.mario.scale.set(6, 6, 6);
+    // this.mario.position.set(-100, 10, 20);
+    // this.mario.rotation.y = 1.5;
+    // this.scene.add(this.mario);
 
     // Loads world
     this.world.position.set(200, 0, 0);
@@ -155,7 +162,7 @@ class Game {
       this.RAF();
       // this.camera.position.x += 0.3;
       // console.log(this.camera.position);
-      this.currentMixer.update(0.01);
+      // this.currentMixer.update(0.01);
       this.renderer.render(this.scene, this.camera);
     });
   }
